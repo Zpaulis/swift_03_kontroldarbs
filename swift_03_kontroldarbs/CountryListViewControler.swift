553df963b2646f6142e02438595c2d1fad9a7e9f
Paulis Zabarovskis <paulis@zabarovski.lv>
2022-05-03 00:32:55 +0300

    First MacBook commit

--- swift_03_kontroldarbs/CountryListViewControler.swift
+++ swift_03_kontroldarbs/CountryListViewControler.swift
@@ -32,19 +32,24 @@ class CountryListViewControler: UIViewController {
     @IBOutlet weak var valsts5karogs: UIImageView!
     @IBOutlet weak var valsts6karogs: UIImageView!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Neracionāla koda paraugs
+        self.countryListTitleLabel.text = countryListTitle
         self.valsts1rinda.text = Array(valstis)[0].key
         self.valsts1karogs.image = UIImage.init(named: Array(valstis)[0].value)
-        
-        
-        
-        
+        self.valsts2rinda.text = Array(valstis)[1].key
+        self.valsts2karogs.image = UIImage.init(named: Array(valstis)[1].value)
+        self.valsts3rinda.text = Array(valstis)[2].key
+        self.valsts3karogs.image = UIImage.init(named: Array(valstis)[2].value)
+        self.valsts4rinda.text = Array(valstis)[3].key
+        self.valsts4karogs.image = UIImage.init(named: Array(valstis)[3].value)
+        self.valsts5rinda.text = Array(valstis)[4].key
+        self.valsts5karogs.image = UIImage.init(named: Array(valstis)[4].value)
+        self.valsts6rinda.text = Array(valstis)[5].key
+        self.valsts6karogs.image = UIImage.init(named: Array(valstis)[5].value)
         
     }
-    
     
 }
 
